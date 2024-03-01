f53cb93cbafc64f6d318eb9a4cf00086410fe37c
Pugsworth <wolst.kyle@gmail.com>
2024-03-01 10:03:39 -0700

    I know how to holster a gun.

---
+++ IKnowHowToHolsterAGun/Patch0.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+
+using Receiver2;
+
+namespace IKnowHowToHolsterAGun
+{
+    [HarmonyPatch(typeof(Receiver2.LocalAimHandler), "AccidentallyPullTrigger")]
+    internal class Patch0
+    {
+        static bool Prefix(Receiver2.LocalAimHandler __instance, GunScript gun, ref float odds)
+        {
+            return false;
+        }
+    }
+}
